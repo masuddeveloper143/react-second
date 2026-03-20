7bcc97e148b1e6b337052193221119201b35156f
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-20 23:39:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  
 
 
-  
+
+
   return (
     <>
-<h1>react second</h1>
+      <h1>react second</h1>
     </>
   )
 }
