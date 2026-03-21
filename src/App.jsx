b14a84d16d272bba9c6a4c91cada91e8bf305927
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-22 00:11:18 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ function App() {
 
   function handleClick() {
     alert("I am cliked");
+  }
+
+  const handleClick3 = () => {
+    alert("click3")
+  }
 
 
-    const handleClick3 = () => {
-      alert("click3")
-    }
-
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
   }
 
   return (
@@ -28,7 +32,11 @@ function App() {
       }}>click me2</button>
 
 
-      <button onClick={handleClick3}></button>
+      <button onClick={handleClick3}>click me3</button>
+
+      <button onClick={() => alert("click me4")}>click me4</button>
+
+      <button onClick={() => handleAdd5(5)}>click me5</button>
 
 
     </>
