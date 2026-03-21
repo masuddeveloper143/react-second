357c0540c9740a0e191b2cd876ecdcf30006db00
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-22 00:28:01 +0800

    Update project

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,4 +2,11 @@ function Counter() {
     const counterStyle = {
         border: '2px solid'
     }
+    return (
+        <div style={counterStyle}>
+            <h3>count: </h3>
+            <button>Add</button>
+
+        </div>
+    )
 }
