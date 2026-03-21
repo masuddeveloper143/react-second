82d136079adbd2476b0d2edc57f458f8d1773a35
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-22 00:45:50 +0800

    Update project

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,6 +2,9 @@
 import { useState } from "react"
 
 export default function Counter() {
+
+    const [count, setCount] = useState(0)
+
     const counterStyle = {
         border: '2px solid red'
     }
