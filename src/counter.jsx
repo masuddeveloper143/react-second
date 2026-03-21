815ceb8da50734ec92453f541d4948bb398a4d80
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-22 00:54:26 +0800

    Update project

--- src/counter.jsx
+++ src/counter.jsx
@@ -3,15 +3,19 @@ import { useState } from "react"
 
 export default function Counter() {
 
-    const [count, setCount] = useState(0)
+    const handleAdd = () => {
+        setCount(5);
+    }
+
+    const [count, setCount] = useState(0);
 
     const counterStyle = {
         border: '2px solid red'
     }
     return (
         <div style={counterStyle}>
-            <h3>count: </h3>
-            <button>Add</button>
+            <h3>count: {count} </h3>
+            <button onClick={handleAdd}>Add</button>
 
         </div>
     )
