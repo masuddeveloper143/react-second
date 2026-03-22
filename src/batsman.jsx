6920f8569ad71235299dca06a9ee546cb1c274d9
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-22 16:00:42 +0800

    Update project

--- src/batsman.jsx
+++ src/batsman.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react"
 
 export default function Batsman() {
-    const [runs, serRuns] = useState(0);
+    let [runs, setRuns] = useState(0);
+
+
+    const handleSingeles = () => {
+        const updatedRuns = runs + 1;
+        setRuns(updatedRuns);
+    }
+
+    const handleSIx = () => {
+        const updatedRuns = runs + 6;
+        setRuns(updatedRuns);
+    }
+
+    const handleFour = () => {
+        const updatedRuns = runs + 6;
+        setRuns(updatedRuns);
+    }
+
+
+
+
     return (
         <div>
             <h1>Plear: Bangla Batsman</h1>
             <h1>Score: {runs}</h1>
+            <button onClick={handleSingeles}> sigeles</button>
+            <button onClick={handleFour}> four</button>
+            <button onClick={handleSIx}>six</button>
+
         </div>
     )
 }
