56254aa1da9309ec918d1fe4915d36b8619334ab
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-29 12:02:40 +0800

    Update project

--- src/countmain.jsx
+++ src/countmain.jsx
@@ -1,10 +1,31 @@
- export default function Countmain(){
-    return(
+import { useState } from "react"
+
+export default function Countmain() {
+
+
+    const [count, setCount] = useState(100);
+
+    const handleUp = () => {
+        setCount(count + 1);
+    }
+
+    const handleDown = () => {
+        setCount(count + 1);
+    }
+
+    const handleRest = () => {
+        setCount(count + 1);
+    }
+
+
+    return (
         <div className="crad">
             <h3>count: </h3>
-            <button>Increase +1</button>
-            <button>Decrease -1</button>
+
+            <button className="button">Increase +1</button>
+            <button className="button">Decrease -1</button>
             <button>Reset</button>
+
         </div>
     )
- }
+}
